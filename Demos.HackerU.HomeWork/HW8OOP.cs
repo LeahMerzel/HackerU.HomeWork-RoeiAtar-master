7551ab26fbed44dcc60eba163c4eee0de1bc6f77
roeiatar87 <118129059+roeiatar87@users.noreply.github.com>
2023-02-25 15:12:39 +0200

    gjhg

--- Demos.HackerU.HomeWork/HW8OOP.cs
+++ Demos.HackerU.HomeWork/HW8OOP.cs
@@ -11,6 +11,7 @@ namespace Demos.HackerU.HomeWork
     {
         public static void Run()
         {
+            #region Q1;
             Person p1 = new Person("markez barnea", "Ashkelon", 35, "Roei Atar");
             p1.DisplayInfoToConsole();
             Customer c1 = new Customer("Maale agat", "markez barnea", 35, "Ashkelon", "Roei Atar");
@@ -19,6 +20,21 @@ namespace Demos.HackerU.HomeWork
             e1.DisplayInfoToConsole();
             Student s1 = new Student("A2 Software", "Atotahim 5", 25, "Netanya", "Matan", StudentType.ComputerScience);
             s1.DisplayInfoToConsole();
+
+            Console.WriteLine("____________________");
+
+            Person[] Person = { p1, c1, e1, s1 };
+
+            foreach (Person p in Person)
+            {
+                p.DisplayInfoToConsole();
+            }
+
+            p1.Age = 10;
+            p1.Address = "new address";
+            p1.City = "New City";
+            p1.DisplayInfoToConsole();
+            #endregion
 
         }
     }

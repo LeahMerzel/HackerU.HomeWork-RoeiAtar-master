1a91c82c04989efc571b829f4ded70c6b9a7c7fd
roeiatar87 <118129059+roeiatar87@users.noreply.github.com>
2023-02-25 12:27:01 +0200

    Add project files.

---
+++ Demos.HackerU.HomeWork/Inhheritance/Person.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demos.HackerU.HomeWork.Inhheritance
+{
+    public class Person
+    {
+
+        public string Addres;
+        public string Name;
+        public int Age;
+        public string City;
+
+        public Person()
+        {
+            Addres = "No Addres";
+            Name = "No Name";
+            Age = 0;
+            City = "No City";
+        }
+
+        public Person(string addres, string name, int age, string city)
+        {
+            Addres = addres;
+            Name = name;
+            Age = age;
+            City = city;
+        }
+        public void DisplayInfoToConsole()
+        {
+            Console.WriteLine($"Name :{Name}\nAge :{Age}\nCity :{City}\nAddres :{Addres}");
+        }
+    }
+}
---
+++ Demos.HackerU/Program.cs
@@ -0,0 +1,41 @@
+﻿using Demos.HackerU.HomeWork;
+namespace Demos.HackerU
+{
+    //Class
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Hw1.Run();
+            Console.WriteLine("-------------");
+
+            Console.WriteLine("-------------");
+
+
+
+            //Hw2.Run();
+
+            //--WAIT FOR USER PRESS ANY KEY--
+            //Hw3.Run();
+            //Hw4.Run();
+
+
+
+            //HW_05
+            //RectangleOOP.Run();
+
+
+
+            //Hw6OOP.Run();
+
+
+            //CategoryOOP.Run();
+
+            //--WAIT FOR USER PRESS ANY KEY--
+            Console.ReadKey();
+        }
+
+
+
+    }
+}
